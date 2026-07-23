8e6b8abb0126c758ea66e6f350c331a36a13e34c
Chejarladhanu18 <chejarladhanalakshmi18@gmail.com>
2026-07-23 13:45:17 +0530

    Use VITE_BASE_URL for Contact API

--- src/services/contactApi.ts
+++ src/services/contactApi.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = "http://127.0.0.1:8000/api"; // change if needed
+const BASE_URL =
+  import.meta.env.VITE_BASE_URL || "http://127.0.0.1:8000/api";
 
 export const submitContactForm = async (data: any) => {
   try {
